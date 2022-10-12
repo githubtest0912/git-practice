cfcb4d46d290888a8c781347c17c2c09b321362b
Selva <githubtest0912>
2022-10-12 18:31:33 +0530

    change setTimeout in promise

--- promise.js
+++ promise.js
@@ -1,5 +1,5 @@
 let promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('resolve done'), 300 )
+    setTimeout(() => resolve('resolve done'), 500 )
 
 }).then((result) => {
     console.log(result)
